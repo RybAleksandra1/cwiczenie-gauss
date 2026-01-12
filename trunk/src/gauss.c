bff90c6916d69adb7288ae628940a3c95ccc56be
nellakarczewicz <01199367@pw.edu.pl>
2026-01-12 13:22:06 +0100

    Faza 3: Zmieniony gauss z pivotem - Nella

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -1,38 +1,66 @@
 #include "gauss.h"
-#include <math.h>  // Może się przydać do abs() w przyszłości, warto dodać
+#include <stdio.h>
+#include <math.h>  // potrzebne do funkcji fabs() 
+
 
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ * wynik 
+ *  0 - eliminacja gaussa zakonczona sukcesem
+ *  1 - macierz osobliwa czyli dzielenie przez 0!!
  */
 int eliminate(Matrix *mat, Matrix *b){
     int n = mat->r;
     int k, i, j;
+    int max_row;     //indeks dla wiersza z najwieksza wartoscia
     double factor;
+    double temp;     // zmienna temp(temporary) pomocnicza do zamiany wierszy
 
-    // Sprawdzamy czy macierze w ogole istnieja i czy maja dobre wymiary 
+    // spr czy macierz jest i czy ma dobry wymiar
     if (!mat || !b || mat->r != mat->c || mat->r != b->r) {
-        return 1; // Błąd błędnych danych
+        fprintf(stderr, "Blad: Nieprawidlowe rozmiary macierzy lub brak danych!\n");
+        return 1; // bledne dane 
     }
 
-    // TRYWIALNA ELIMINACJA GAUSSA 
-
-    // petla po kolumnach (krokach eliminacji)
+    // tu mamy glowna petle eliminacji gaussa
+    // petla idzie po kolumnach
     for(k = 0; k < n - 1; k++) {
         
-        // Sprawdzenie czy element na diagonalnej nie jest 0
-        if (mat->data[k][k] == 0) {
-            return 1; // Blad, mamy dzielenie przez 0 (macierz osobliwa)
+       // tu cos nowego - wybor elementu glownego 
+        // Szukamy wiersza z najwieksza wartosica bezwzgledna w kolumnie k
+        max_row = k;
+        for (i = k + 1; i < n; i++) {
+            if (fabs(mat->data[i][k]) > fabs(mat->data[max_row][k])) {
+                max_row = i;
+            }
         }
 
-        // petla po wierszach 
-	for(i = k + 1; i < n; i++) {
+        // jak znajdziemy lepszy wiersz zamieniamy go
+        if (max_row != k) {
+            // Zamiana wierszy w macierzy A 
+            for (j = k; j < n; j++) { 
+                temp = mat->data[k][j];
+                mat->data[k][j] = mat->data[max_row][j];
+                mat->data[max_row][j] = temp;
+            }
             
-            // Obliczenie wspolczynnika, przez ktory mnozymy wiersz k
+            // Zamiana wierszy w wektorze b (wazny krok!!)
+            temp = b->data[k][0];
+            b->data[k][0] = b->data[max_row][0];
+            b->data[max_row][0] = temp;
+        }
+       
+        // Sprawdzenie czy po zamianie element na diagonalnej nadal nie jest zerowy
+        if (mat->data[k][k] == 0) {
+            return 1; // Blad, macierz osobliwa
+        }
+
+        // Dalsza czesc taka sama jak dla trywialnej elimincji gaussa
+        for(i = k + 1; i < n; i++) {
+            
+            // Obliczenie wspolczynnika
             factor = mat->data[i][k] / mat->data[k][k];
 
             // Odejmowanie wiersza k od wiersza i w macierzy A
-            // Zaczynamy od j=k, bo elementy wczesniej sa juz wyzerowane
             for(j = k; j < n; j++) {
                 mat->data[i][j] -= factor * mat->data[k][j];
             }
@@ -42,5 +70,5 @@ int eliminate(Matrix *mat, Matrix *b){
         }
     }
 
-    return 0; // Sukces
+    return 0; // oznacza sukces
 }
