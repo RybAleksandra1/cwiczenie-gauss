5c47677d09621cc78382741a64e4fc84a22b54a9
RybAleksandra1 <01206822@pw.edu.pl>
2026-01-12 13:17:17 +0100

    Faza 2: Implementacja algorytmu podstawiania wstecznego

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -5,18 +5,40 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	// Weryfikacja rozmiarow macierzy - macierz 'mat' musi byc kwadratowa (r x r)
+	// oraz macierze 'b' i 'x' muszą mieć tyle samo wierszy co 'mat' 
+	if (mat->r != mat->c || mat->r != b->r || mat->r != x->r) {
+		return 2; // Błąd nieprawidłowych rozmiarów macierzy
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	// Przypisujemy rozmiar macierzy do zmiennej n dla czytelności
+    int n = mat->r; 
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	// Algorytm podstawiania wstecznego 
 
-				return 0;
+	// Petla ktora przechodzi od ostatniego wiersza (n-1) do pierwszego (0)
+	for (int i = n - 1; i >= 0; i--) {
+		// Zmienna pomocnicza s, w której będziemy gromadzić sumę znanych
+		// elementów, które "przenosimy na drugą stronę" równania
+        double s = 0;
+
+		// Sumujemy iloczyny znanych już wartości x[j] i współczynników macierzy
+        for (int j = i + 1; j < n; j++) {
+			// Mnożymy współczynnik macierzy przez obliczoną już niewiadomą i dodajemy do sumy s
+            s += mat->data[i][j] * x->data[j][0];
+        }
+
+        // Sprawdzamy, czy nie dzielimy przez zero (element na diagonali rowny zero)
+        if (mat->data[i][i] == 0) {
+            return 1;
+        }
+
+        // Obliczamy wartość x[i] (niewiadomą)
+		// b->data[i][0] - wyraz wolny z prawej strony równania
+        x->data[i][0] = (b->data[i][0] - s) / mat->data[i][i];
+
+	}
+
+	// Wsteczne podstawienie zakonczone sukcesem
+	return 0;
 }
-
-
